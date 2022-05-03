f58a292b330dc59c345aa701f77e1281fb3a750a
Frazier Rodgers <frazierr04@gmail.com>
2022-05-03 18:22:24 -0500

    corrected path to css and js

--- js/main.js
+++ js/main.js
@@ -9,38 +9,38 @@ document.querySelector("#startButton").addEventListener('click', startGame)
 
 //after startButton is clicked, increase turnCounter, update current player turn with turnCounter, hide startButton, reveal game board
 function startGame(){
-  turnCounter++
-  checkTurn()
-  document.querySelector("#startButton").classList.add("hidden")
-  document.querySelector(".board").classList.remove("hidden")
+	turnCounter++
+	checkTurn()
+	document.querySelector("#startButton").classList.add("hidden")
+	document.querySelector(".board").classList.remove("hidden")
 }
 
 // update the current player's turn
 function checkTurn(){
-  if (turnCounter % 2 === 0){
-    document.querySelector("#playerTurn").innerText = "X's turn"
-  } else {
-    document.querySelector("#playerTurn").innerText = "O's turn"
-  }
+	if (turnCounter % 2 === 0){
+		document.querySelector("#playerTurn").innerText = "X's turn"
+	} else {
+		document.querySelector("#playerTurn").innerText = "O's turn"
+	}
 }
 
 // constructor to addEventListener to squares, make function to addValue to square when clicked, update winPattern array with values added with click, check if any winPattern arrays are true, increase turnCounter, update current player turn with turnCounter, or alert if space is already filled.
 class makeSquare{
-  constructor (position, squareNum){
-    this.position = position
-    this.squareNum = squareNum
-    position.addEventListener('click', addValue =>{
-      if(this.position.innerText === ""){
-        turnCounter % 2 === 0 ? this.position.innerText = "X" : this.position.innerText = "O";
-        updateArray()
-        checkWin()
-        turnCounter++
-        checkTurn()
-      } else{
-        alert("space is taken already")
-      }
-    })
-  }
+	constructor (position, squareNum){
+		this.position = position
+		this.squareNum = squareNum
+		position.addEventListener('click', addValue =>{
+			if(this.position.innerText === ""){
+				turnCounter % 2 === 0 ? this.position.innerText = "X" : this.position.innerText = "O";
+				updateArray()
+				checkWin()
+				turnCounter++
+				checkTurn()
+			} else{
+				alert("space is taken already")
+			}
+		})
+	}
 }
 
 //actual constructing of clickable squares
@@ -57,13 +57,13 @@ let square9 = new makeSquare(document.querySelector(".square9"), '.square9')
 
 // // constructor to make winPattern arrays
 // class winPattern{
-//  constructor (s1, s2, s3){
-//    this.s1 = s1
-//    this.s2 = s2
-//    this.s3 = s3
+// 	constructor (s1, s2, s3){
+// 		this.s1 = s1
+// 		this.s2 = s2
+// 		this.s3 = s3
 
-//    this.winArray = [this.s1, this.s2, this.s3]
-//  }
+// 		this.winArray = [this.s1, this.s2, this.s3]
+// 	}
 // }
 
 //declaring winPattern arrays as variables
@@ -80,61 +80,61 @@ let fullGrid = [square1.position.innerText, square2.position.innerText, square3.
 
 //function to update WinPattern arrays with correct values after clicking square
 function updateArray(){
-  row1 = [square1.position.innerText, square2.position.innerText, square3.position.innerText]
-  row2 = [square4.position.innerText, square5.position.innerText, square6.position.innerText]
-  row3 = [square7.position.innerText, square8.position.innerText, square9.position.innerText]
-  col1 = [square1.position.innerText, square4.position.innerText, square7.position.innerText]
-  col2 = [square2.position.innerText, square5.position.innerText, square8.position.innerText]
-  col3 = [square3.position.innerText, square6.position.innerText, square9.position.innerText]
-  diag1 = [square1.position.innerText, square5.position.innerText, square9.position.innerText]
-  diag2 = [square7.position.innerText, square5.position.innerText, square3.position.innerText]
+	row1 = [square1.position.innerText, square2.position.innerText, square3.position.innerText]
+	row2 = [square4.position.innerText, square5.position.innerText, square6.position.innerText]
+	row3 = [square7.position.innerText, square8.position.innerText, square9.position.innerText]
+	col1 = [square1.position.innerText, square4.position.innerText, square7.position.innerText]
+	col2 = [square2.position.innerText, square5.position.innerText, square8.position.innerText]
+	col3 = [square3.position.innerText, square6.position.innerText, square9.position.innerText]
+	diag1 = [square1.position.innerText, square5.position.innerText, square9.position.innerText]
+	diag2 = [square7.position.innerText, square5.position.innerText, square3.position.innerText]
 
-  fullGrid = [square1.position.innerText, square2.position.innerText, square3.position.innerText, square4.position.innerText, square5.position.innerText, square6.position.innerText, square7.position.innerText, square8.position.innerText, square9.position.innerText]
+	fullGrid = [square1.position.innerText, square2.position.innerText, square3.position.innerText, square4.position.innerText, square5.position.innerText, square6.position.innerText, square7.position.innerText, square8.position.innerText, square9.position.innerText]
 }
 
 //check if any winPattern arrays are all Xs or Os, reveal playAgain button
 function checkWin(){
-  function isX(element){
-    return element === "X"
-  }
-  function isO(element) {
-    return element === "O"
-  }
-  if(row1.every(isX) || row2.every(isX) || row3.every(isX) || col1.every(isX) || col2.every(isX) || col3.every(isX) || diag1.every(isX) || diag2.every(isX))
-  {
-    document.querySelector("#resultsText").innerText = "X wins!"
-    playAgainPrompt()
-  }
-  else if(row1.every(isO) || row2.every(isO) || row3.every(isO) || col1.every(isO) || col2.every(isO) || col3.every(isO) || diag1.every(isO) || diag2.every(isO))
-  {
-    document.querySelector("#resultsText").innerText = "O wins!"
-    playAgainPrompt()
-  } else {
-    checkScratch()
-  }
+	function isX(element){
+		return element === "X"
+	}
+	function isO(element) {
+		return element === "O"
+	}
+	if(row1.every(isX) || row2.every(isX) || row3.every(isX) || col1.every(isX) || col2.every(isX) || col3.every(isX) || diag1.every(isX) || diag2.every(isX))
+	{
+		document.querySelector("#resultsText").innerText = "X wins!"
+		playAgainPrompt()
+	}
+	else if(row1.every(isO) || row2.every(isO) || row3.every(isO) || col1.every(isO) || col2.every(isO) || col3.every(isO) || diag1.every(isO) || diag2.every(isO))
+	{
+		document.querySelector("#resultsText").innerText = "O wins!"
+		playAgainPrompt()
+	} else {
+		checkScratch()
+	}
 }
 
 //updated checkScratch function
 function checkScratch(){
-  if(fullGrid.every(function isFull(element){
-    return element != ""
-  })){
-    document.querySelector("#resultsText").innerText = "SCRATCH"
-    playAgainPrompt()
-  }
+	if(fullGrid.every(function isFull(element){
+		return element != ""
+	})){
+		document.querySelector("#resultsText").innerText = "SCRATCH"
+		playAgainPrompt()
+	}
 }
 
 //add resetButton eventListener & function
 document.querySelector("#playAgain").addEventListener("click", resetGame)
 
 function resetGame(){
-  document.location.reload()
+	document.location.reload()
 }
 
 //condensed some repeated code from above with function
 function playAgainPrompt(){
-  document.querySelector("#playAgain").classList.remove("hidden")
-  document.querySelector(".board").style.pointerEvents = "none"
+	document.querySelector("#playAgain").classList.remove("hidden")
+	document.querySelector(".board").style.pointerEvents = "none"
 }
 
 //THINGS TO ADD
